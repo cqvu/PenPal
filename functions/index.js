36ee227e471a7da945fd58563b93824ce4c00ce7
vivian <vyl003@ucsd.edu>
2020-08-09 11:59:02 -0700

    abandon backend

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,71 @@
 //import firebase from "../penpal-app/src/components/firebase";
-const firebase = require( "../penpal-app/src/components/firebase");
+//const firebase = require( "../penpal-app/src/components/firebase");
 //const firebase = require('firebase');
 const express = require('express');
 const cors = require('cors');
-
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const rp = require("request-promise");
 admin.initializeApp();
 
-const functions = require('firebase-functions');
-// Initialize express
-// const app = express();
+// fbObject = new firebase();
+// var db = fbObject.db;
 
-// app.use(cors({ origin: true }));
-
-// app.post('/usrResponse', function (req, res) {
-//   console.log("webhook hit")
-//     var body = req.body;
-//     // var trackingNumber = body.msg.tracking_number;
-//     // var slug = body.msg.slug;
-//     // var token = body.msg.unique_token;
-
-//   //  console.log(trackingNumber, slug, token);
-//     console.log(body);
-//     res.send(usrResponse.create();
+const authToken = "HmZ6SefEtPnqRFJZEqd7oTxvv3KjVA358YuWUQ4A5oUx";
+const functionURL = "https://vyl003.typeform.com/forms/CJ2irtPt/responses?included_response_ids=owlgwbiizeqzvp8tu8o6owlgl9agg0ca";
+// exports.usrResponse = functions.https.onRequest((req, res) => {
+// 	var usrToken = req.body.form_response.token;
+// 	var map = new Map();
+// 	map["choice 1"] = req.body.form_response[0].choice.label;
+// 	map["choice 2"] = req.body.form_response[1].choice.label;
+// 	map["email"] = body.form_response.answers[2].email;
+//     db.collection("Matches").doc(email).set({
+// 		m
+// 	})
+//     .then(function(){
+//     	console.log("Document written in Matches");
+//     	res.json({
+// 	        status: 200,
+// 	        token: usrToken,
+// 	        message: 'ok got it!'
+// 	    });
+//     })
+//     .catch(function(error){
+//     	console.error("Error in writing document to matches: ", error);
+//     })
 // });
-fbObject = new firebase();
-var db = fbObject.db;
-exports.usrResponse = functions.https.onRequest((req, res) => {
-	var email = fbObject.getCurrentEmail;
-    var usrToken = req.body.form_response.token;
-    console.log(usrToken);
-    db.collection("Matches").doc(email).set({
-    	token: usrToken
-    })
-    .then(function(){
-    	console.log("Document written in Matches");
-    	res.json({
-	        status: 200,
-	        token: usrToken,
-	        message: 'ok got it!'
-	    });
-    })
-    .catch(function(error){
-    	console.error("Error in writing document to matches: ", error);
-    })
-});
 
+// exports.callingFunction =  functions.https.onRequest((req, res) => {
+// 	var options = {
+// 		uri: functionURL,
+// 		method: 'GET',
+// 		headers: {Authorization: `bearer ${authToken}`},
+// 		json: true // Automatically stringifies the body to JSON
+
+// 	};
+// 	rp(options)
+//     .then(function (parsebody) {
+// 		console.log('User response: ', parsebody);
+// 		res.json({
+// 	        status: 200,
+// 	        message: 'ok got it!'
+// 	    });
+//     })
+//     .catch(function (err) {
+//         console.error('Error:' , err);
+// 	});
+// 	res.end();
+	// Provide the token in the request to the receiving function
+	// try {
+	//   const functionResponse = await get(functionURL, {
+	// 	headers: {Authorization: `bearer ${authToken}`},
+	//   });
+	//   res.status(200).send(functionResponse.data);
+	// } catch (err) {
+	//   console.error(err);
+	//   res.status(500).send('An error occurred! See logs for more details.');
+	// }
+//   });
 
 //Automatically allow cross-origin requests
 // app.use(cors({ origin: true }));
